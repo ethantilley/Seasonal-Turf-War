bfebfdf7e7c279c33981bcb146ec158185d00e1c
Ethan Tilley <1008464@student.sae.edu.au>
2018-05-13 10:12:47 +1000

    My Unity Starter Pack Added

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+
+    public void LoadLevel (string sceneName)
+    {
+        if (sceneName != "Exit")
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        else
+            Application.Quit();
+    }
+
+
+
+}
