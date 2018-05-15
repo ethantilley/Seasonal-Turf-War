cbf99d700fc3e1b7739cdd0793027e1ce09b7cb0
Ethan Tilley <1008464@student.sae.edu.au>
2018-05-15 11:03:53 +1000

    Multiple Controller Jumping Input Implemented. Awaiting Testing.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -16,19 +16,27 @@ public class InputManager : MonoBehaviour {
     private UnityStandardAssets._2D.PlatformerCharacter2D m_Character;
     public float playerSpeed = 5;
     float x_Move = 0;
+    bool jumped;
     // Use this for initialization
     void Start () {
         m_Character = GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    private void Update()
+    {
         CheckControllerMovement();
+    
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        
 
 
-        m_Character.Move(x_Move, false, false);
-        
-        
+        m_Character.Move(x_Move, false, jumped);
+
+        jumped = false;
     }
 
     void CheckControllerMovement()
@@ -38,15 +46,23 @@ public class InputManager : MonoBehaviour {
         {
             case Player.Player1:
                 x_Move = Input.GetAxis("Controller1_Horizontal");
+                if(!jumped)
+                    jumped = Input.GetButtonDown("Controller1_Jump");
                 break;
             case Player.Player2:
                 x_Move = Input.GetAxis("Controller2_Horizontal");
+                if (!jumped)
+                    jumped = Input.GetButtonDown("Controller2_Jump");
                 break;
             case Player.Player3:
                 x_Move = Input.GetAxis("Controller3_Horizontal");
+                if (!jumped)
+                    jumped = Input.GetButtonDown("Controller3_Jump");
                 break;
             case Player.Player4:
                 x_Move = Input.GetAxis("Controller4_Horizontal");
+                if (!jumped)
+                    jumped = Input.GetButtonDown("Controller4_Jump");
                 break;
             default:
                 break;
