c9bc920fc9ae7ad23e28072e5ffd4aefa32b4d83
Ethan Tilley <1008464@student.sae.edu.au>
2018-05-15 12:35:20 +1000

    Aiming With Right Joystick Implemented. Awaiting Testing.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
     public enum Player
     {
@@ -17,22 +19,53 @@ public class InputManager : MonoBehaviour {
     public float playerSpeed = 5;
     float x_Move = 0;
     bool jumped;
+
+    float rstickX;
+    float rstickY;
+    private float cursorspeed = 5;
+    Vector2 direction;
+    float ConAngle;
+    public float offset;
+    public float angle;
+    public Image imgCusor;
+    public Transform gun;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         m_Character = GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>();
     }
 
     private void Update()
     {
         CheckControllerMovement();
-    
 
+        if (imgCusor.transform.position == gameObject.transform.position)
+        {
+            imgCusor.gameObject.SetActive(false);
+        }
+        else
+        {
+            imgCusor.gameObject.SetActive(true);
+        }
+       
+
+       // imgCusor.transform.rotation = Quaternion.(ConAngle, Vector3.forward);
     }
 
     // Update is called once per frame
-    void FixedUpdate () {
-        
+    void FixedUpdate()
+    {
 
+        //Timesing those values by time deltatime (the time i took to complet the last frame) and a adjustable cursor speed 
+        rstickX *= Time.deltaTime + cursorspeed;
+        rstickY *= Time.deltaTime + cursorspeed;
+        direction = new Vector2(rstickX, rstickY);
+        // Moving the cursor position 
+        ConAngle = Mathf.Atan2(rstickY, rstickX) * Mathf.Rad2Deg;
+        // Set the guns rotation the forward angle position 
+        //uiScript.imgCusor.transform.Translate(0, rsticky, 0);
+        imgCusor.transform.position = gun.position + (Vector3)direction.normalized * offset;
+        gun.rotation = Quaternion.AngleAxis(ConAngle, Vector3.forward);
 
         m_Character.Move(x_Move, false, jumped);
 
@@ -41,34 +74,50 @@ public class InputManager : MonoBehaviour {
 
     void CheckControllerMovement()
     {
-     
+
         switch (selectedPlayer)
         {
             case Player.Player1:
                 x_Move = Input.GetAxis("Controller1_Horizontal");
-                if(!jumped)
+
+                rstickX = Input.GetAxis("Controller1_RStickX");
+                rstickY = Input.GetAxis("Controller1_RStickY");
+
+                if (!jumped)
                     jumped = Input.GetButtonDown("Controller1_Jump");
                 break;
             case Player.Player2:
                 x_Move = Input.GetAxis("Controller2_Horizontal");
+
+                rstickX = Input.GetAxis("Controller2_RStickX");
+                rstickY = Input.GetAxis("Controller2_RStickY");
+
                 if (!jumped)
                     jumped = Input.GetButtonDown("Controller2_Jump");
                 break;
             case Player.Player3:
                 x_Move = Input.GetAxis("Controller3_Horizontal");
+
+                rstickX = Input.GetAxis("Controller3_RStickX");
+                rstickY = Input.GetAxis("Controller3_RStickY");
+
                 if (!jumped)
                     jumped = Input.GetButtonDown("Controller3_Jump");
                 break;
             case Player.Player4:
                 x_Move = Input.GetAxis("Controller4_Horizontal");
+
+                rstickX = Input.GetAxis("Controller4_RStickX");
+                rstickY = Input.GetAxis("Controller4_RStickY");
+
                 if (!jumped)
                     jumped = Input.GetButtonDown("Controller4_Jump");
                 break;
             default:
                 break;
         }
-        
-       
+
+
         print(GetComponent<Rigidbody2D>().velocity + " : " + x_Move);
     }
 
