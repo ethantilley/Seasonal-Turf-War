ad504cc4a89eff3aacc98a63f7f581b42a0ff92a
Ethan Tilley <1008464@student.sae.edu.au>
2018-05-18 10:01:50 +1000

    Font and canvas editing

---
+++ Assets/Scripts/FontFixer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FontFixer : MonoBehaviour {
+    public Font[] fonts;
+
+	// Use this for initialization
+	void Start () {
+        foreach (var item in fonts)
+        {
+            item.material.mainTexture.filterMode = FilterMode.Point;
+        }
+	}
+	
+	
+}
