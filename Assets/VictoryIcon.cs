1a0af8fa5a0d66f361eb64a0818a4aee08785570
Ethan Tilley <1008464@student.sae.edu.au>
2018-05-18 11:04:55 +1000

    Did Jesse's job for him. smile. *Made the victory icons display*

---
+++ Assets/VictoryIcon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryIcon : MonoBehaviour {
+
+    public Sprite autumn, spring, summer, winter;
+
+    private Image image;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+
+    private void OnEnable()
+    {
+        image = GetComponent<Image>();
+        
+        switch (GameManager.instance.CalculateWinningPlayer())
+        {
+           
+            case "Autumn":
+                image.sprite = autumn;
+                break;
+            case "Spring":
+                image.sprite = spring;
+                break;
+            case "Summer":
+                image.sprite = summer;
+                break;
+            case "Winter":
+                image.sprite = winter;
+                break;
+
+            default:
+                Debug.LogWarning("What... ");
+                break;
+        }
+    }
+}
